e4488c309347577606fe40a39179b7be32a2c173
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-06-15 10:18:33 +0200

    inline admin

--- quiz/main/admin.py
+++ quiz/main/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
+from quiz.admin_helpers import InlineEditLinkMixin
 from quiz.main.models import Quiz, Question, Choice
 
-admin.site.register([Quiz, Question, Choice])
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline, ]
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class QuestionInline(InlineEditLinkMixin, admin.TabularInline):
+    model = Question
+    fields = ('text', 'edit_details')
+
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline, ]
+
+admin.site.register(Quiz, QuizAdmin)
--- quiz/main/models.py
+++ quiz/main/models.py
@@ -7,6 +7,9 @@ class Quiz(models.Model):
     start = models.DateTimeField()
     end = models.DateTimeField()
 
+    def __unicode__(self):
+        return self.title
+
     class Meta:
         verbose_name_plural = 'quizes'
 
@@ -15,9 +18,14 @@ class Question(models.Model):
     quiz = models.ForeignKey(Quiz)
     text = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
-    correct_answer = models.OneToOneField(Question,
-                                          related_name='correct_answer')
+    correct_answer = models.BooleanField(default=False)
     text = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.text
