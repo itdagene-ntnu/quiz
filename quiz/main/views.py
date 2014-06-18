0fbedd2865ccd4b2c8fb978885c88ee937db568a
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-06-18 22:31:56 +0200

    add no quiz template

--- quiz/main/views.py
+++ quiz/main/views.py
@@ -6,5 +6,7 @@ from quiz.main.models import Quiz
 def landing(req):
     current_quiz = Quiz.objects.get(start__lte=timezone.now(),
                                     end__gte=timezone.now())
-    print current_quiz
-    return render(req, 'landing.html', {'current_quiz': current_quiz})
+    if current_quiz:
+        return render(req, 'landing.html', {'current_quiz': current_quiz})
+    else:
+        return render(req, 'no_quiz.html')
